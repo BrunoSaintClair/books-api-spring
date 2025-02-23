1166161b8888c512e66eb1e6bbf906c16571d627
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2025-02-23 14:45:32 -0300

    fix: Adiciona annotation @Repository e altera o tipo de id

--- src/main/java/com/api/books_registration/Repositories/BookRepository.java
+++ src/main/java/com/api/books_registration/Repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.api.books_registration.Repositories;
 
 import com.api.books_registration.Entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookRepository extends JpaRepository<Book, String> {
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }

02eb18d9e37daae8858d362019926de50a9ff79d
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2025-02-23 22:01:33 -0300

    feat: cria endpoint que lista livros

--- src/main/java/com/api/books_registration/Controllers/BookController.java
+++ src/main/java/com/api/books_registration/Controllers/BookController.java
@@ -1,9 +1,13 @@
 package com.api.books_registration.Controllers;
 
+import com.api.books_registration.Entities.Book;
 import com.api.books_registration.Services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -11,4 +15,10 @@ public class BookController {
 
     @Autowired
     private BookService service;
+
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return service.getAllBooks();
+    }
+
 }
--- src/main/java/com/api/books_registration/Services/BookService.java
+++ src/main/java/com/api/books_registration/Services/BookService.java
@@ -1,12 +1,19 @@
 package com.api.books_registration.Services;
 
+import com.api.books_registration.Entities.Book;
 import com.api.books_registration.Repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository repository;
+
+    public List<Book> getAllBooks(){
+        return repository.findAll();
+    }
 }
